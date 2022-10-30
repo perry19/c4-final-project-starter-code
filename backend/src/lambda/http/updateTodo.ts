9ac035a0c741afcc2ee646bb1539fa6a5a5fa4a1
perry19 <fopaarmel@gmail.com>
2022-10-30 14:34:13 +0100

    (feat-severless-todo): implemented update todo method

--- backend/src/lambda/http/updateTodo.ts
+++ backend/src/lambda/http/updateTodo.ts
@@ -1,26 +1,48 @@
-// import 'source-map-support/register'
+import 'source-map-support/register'
 
-// import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
-// import * as middy from 'middy'
-// import { cors, httpErrorHandler } from 'middy/middlewares'
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+import * as middy from 'middy'
+import { cors, httpErrorHandler } from 'middy/middlewares'
 
-// import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest'
+import { updateTodo } from '../../businessLogic/todos'
+import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest'
+import { getUserId } from '../utils'
+import { createLogger } from '../../utils/logger'
 
-// export const handler = middy(
-//   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-//     const todoId = event.pathParameters.todoId
-//     const updatedTodo: UpdateTodoRequest = JSON.parse(event.body)
-//     // TODO: Update a TODO item with the provided id using values in the "updatedTodo" object
+const logger = createLogger('createTodo');
 
+export const handler = middy(async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  const todoId = event.pathParameters.todoId;
+  const updatedTodo: UpdateTodoRequest = JSON.parse(event.body);
+  const userId = getUserId(event);
 
-//     return undefined
-//   }
-// )
+  try {
+    await updateTodo(userId, todoId, updatedTodo);
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true
+      },
+      body: '',
+    };
+  } catch (e) {
+    logger.error('Error updating todo', e);
+    return {
+      statusCode: 500,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true
+      },
+      body: 'Internal Server Error',
+    };
+  }
+});
 
-// handler
-//   .use(httpErrorHandler())
-//   .use(
-//     cors({
-//       credentials: true
-//     })
-//   )
+handler
+.use(httpErrorHandler())
+.use(
+  cors({
+    credentials: true
+  })
+)
