3d681aae874f902489b8798bcf3ef6db8ffa03d6
perry19 <fopaarmel@gmail.com>
2022-10-30 14:40:25 +0100

    (feat-severless-todo): implemented generate upload url

--- backend/src/lambda/http/generateUploadUrl.ts
+++ backend/src/lambda/http/generateUploadUrl.ts
@@ -1,26 +1,44 @@
-// import 'source-map-support/register'
+import 'source-map-support/register'
 
-// import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
-// import * as middy from 'middy'
-// import { cors, httpErrorHandler } from 'middy/middlewares'
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+import * as middy from 'middy'
+import { cors, httpErrorHandler } from 'middy/middlewares'
 
-// import { createAttachmentPresignedUrl } from '../../businessLogic/todos'
-// import { getUserId } from '../utils'
+import { createAttachmentPresignedUrl } from '../../businessLogic/todos'
+import { getUserId } from '../utils';
+import {createLogger} from "../../utils/logger";
 
-// export const handler = middy(
-//   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-//     const todoId = event.pathParameters.todoId
-//     // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
-    
+const logger = createLogger('uploadFile');
 
-//     return undefined
-//   }
-// )
+export const handler = middy(
+  async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    const todoId = event.pathParameters.todoId
+    const userId = getUserId(event);
 
-// handler
-//   .use(httpErrorHandler())
-//   .use(
-//     cors({
-//       credentials: true
-//     })
-//   )
+    try {
+      const presignedUrl = await createAttachmentPresignedUrl(userId, todoId);
+
+      return {
+        statusCode: 201,
+        body: JSON.stringify({
+          uploadUrl: presignedUrl,
+        })
+      }
+    }
+    catch (e) {
+      logger.error('Error creating presigned url', e);
+      return {
+        statusCode: 500,
+        body: 'Internal Server Error',
+      };
+    }
+  }
+)
+
+handler
+.use(httpErrorHandler())
+.use(
+  cors({
+    credentials: true
+  })
+)
