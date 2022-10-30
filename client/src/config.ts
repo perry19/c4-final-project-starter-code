267dc35b90249ba1e691f4b50b264a713fccc1f3
perry19 <fopaarmel@gmail.com>
2022-10-30 14:23:46 +0100

    (feat-serveless-todo-ui): added domain, api, and client id

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,11 +1,11 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '...'
+const apiId = '54sooggqk0'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map. For example:
   // domain: 'dev-nd9990-p4.us.auth0.com',
-  domain: '...',            // Auth0 domain
-  clientId: '...',          // Auth0 client id
+  domain: 'dev-z7dha2gw8gn2nll7.us.auth0.com',            // Auth0 domain
+  clientId: 'cc8o1ClMj02j08UlZ9ZixLuD2ygC36Sm',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
